960e48dec1b850b959e1c1efbac6f67f1d1e3a2a
openvela-robot <openvela-robot@xiaomi.com>
2021-09-14 15:51:13 +0800

    Merge branch 'develop' of github.com:EmbeddedRPC/erpc into feature/staticRPMSGApi

--- erpc_c/infra/erpc_transport_arbitrator.h
+++ erpc_c/infra/erpc_transport_arbitrator.h
@@ -129,6 +129,10 @@ public:
      */
     virtual bool hasMessage(void) override;
 
+protected:
+    Transport *m_sharedTransport; //!< Transport being shared through this arbitrator.
+    Codec *m_codec;               //!< Codec used to read incoming message headers.
+
     /*!
      * @brief Request info for a client trying to receive a response.
      */
@@ -150,20 +154,9 @@ public:
         ~PendingClientInfo(void);
     };
 
-protected:
-    Transport *m_sharedTransport; //!< Transport being shared through this arbitrator.
-    Codec *m_codec;               //!< Codec used to read incoming message headers.
-
     PendingClientInfo *m_clientList;     //!< Active client receive requests.
     PendingClientInfo *m_clientFreeList; //!< Unused client receive info structs.
     Mutex m_clientListMutex;             //!< Mutex guarding the client active and free lists.
-
-    /*!
-     * @brief Create a Pending Client object.
-     *
-     * @return PendingClientInfo* Return created object.
-     */
-    PendingClientInfo *createPendingClient(void);
 
     /*!
      * @brief This function adds pending client.
--- erpc_c/port/erpc_setup_extensions_freertos.cpp
+++ erpc_c/port/erpc_setup_extensions_freertos.cpp
@@ -7,7 +7,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "erpc_manually_constructed.h"
 #include "erpc_setup_extensions.h"
 #include "erpc_threading.h"
 
@@ -17,11 +16,9 @@ using namespace erpc;
 
 static Semaphore *s_erpc_call_in_progress = NULL;
 static TimerHandle_t s_erpc_call_timer_cb = NULL;
-#if ERPC_ALLOCATION_POLICY == ERPC_ALLOCATION_POLICY_STATIC
+#if configSUPPORT_STATIC_ALLOCATION
 static StaticTimer_t s_static_erpc_call_timer_cb;
 #endif
-
-ERPC_MANUALLY_CONSTRUCTED_STATIC(Semaphore, s_semaphore);
 
 void erpc::erpc_pre_cb_default(void)
 {
@@ -49,16 +46,10 @@ void erpc_init_call_progress_detection_default(
     erpc_call_timer_cb_default_t erpc_call_timer_cb = erpc_call_timer_cb_default,
     uint32_t waitTimeMs = (5U * 60U * 1000U))
 {
-    const uint32_t semaphoreCount = 1;
-#if ERPC_ALLOCATION_POLICY == ERPC_ALLOCATION_POLICY_DYNAMIC
-    s_erpc_call_in_progress = new Semaphore(semaphoreCount);
-#elif ERPC_ALLOCATION_POLICY == ERPC_ALLOCATION_POLICY_STATIC
-    s_semaphore.construct(semaphoreCount);
-    s_erpc_call_in_progress = s_semaphore.get();
-#endif
+    s_erpc_call_in_progress = new Semaphore(1);
     assert(s_erpc_call_in_progress && "Creating eRPC semaphore failed.");
 
-#if ERPC_ALLOCATION_POLICY == ERPC_ALLOCATION_POLICY_STATIC
+#if configSUPPORT_STATIC_ALLOCATION
     s_erpc_call_timer_cb = xTimerCreateStatic("Erpc client call timer", waitTimeMs / portTICK_PERIOD_MS, pdFALSE, NULL,
                                               erpc_call_timer_cb, &s_static_erpc_call_timer_cb);
 #else
@@ -72,7 +63,7 @@ void erpc_deinit_call_progress_detection_default(void)
 {
     if (s_erpc_call_in_progress != NULL)
     {
-#if ERPC_ALLOCATION_POLICY == ERPC_ALLOCATION_POLICY_DYNAMIC
+#if ERPC_ALLOCATION_POLICY == ERPC_DYNAMIC_POLICY
         delete s_erpc_call_in_progress;
 #endif
         s_erpc_call_in_progress = NULL;
