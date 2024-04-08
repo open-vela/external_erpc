e20b02702fd21d67fe95d15abc814f5eebbdfb81
zhangbo56 <zhangbo56@xiaomi.com>
2024-04-08 10:05:05 +0800

    erpc: Add delete function for erpcgen tool
    So add -d or --delete will remove all files generated by erpcgen.
    
    Signed-off-by: zhangbo56 <zhangbo56@xiaomi.com>
    Signed-off-by: cuiziwei <cuiziwei@xiaomi.com>

--- erpcgen/src/erpcgen.cpp
+++ erpcgen/src/erpcgen.cpp
@@ -25,6 +25,7 @@
 #include <stdexcept>
 #include <vector>
 #include <string.h>
+#include <dirent.h>
 #include <linux/limits.h>
 
 /*!
@@ -55,6 +56,7 @@ static const char *k_optionsDefinition[] = { "?|help",
                                              "g:generate <language>",
                                              "c:codec <codecType>",
                                              "p:package <packageName>",
+                                             "d|delete",
                                              "a|add prefix",
                                              NULL };
 
@@ -69,6 +71,7 @@ const char k_usageText[] =
   -g/--generate <language>     Select the output language (default is C)\n\
   -c/--codec <codecType>       Specify used codec type\n\
   -p/--package <packageName>   Java app package (com.example.app) (only for Java)\n\
+  -d/--delete                  Delete erpc gen files\n\
   -a/--addprefix               Add client or server prefix for function name\n\
 \n\
 Available languages (use with -g option):\n\
@@ -122,6 +125,7 @@ protected:
     InterfaceDefinition::codec_t m_codec; /*!< Used codec type. */
     string m_javaPackageName;             /*!< Used java package. */
     bool m_addPrefix;                     /*!< Flag hints for adding prefix for c/s func name. */
+    bool m_isDelete;                      /*!< Flag hints for deleting erpc gen files. */
 
 public:
     /*!
@@ -134,7 +138,7 @@ public:
      */
     erpcgenTool(int argc, char *argv[]) :
     m_argc(argc), m_argv(argv), m_logger(0), m_verboseType(verbose_type_t::kWarning), m_outputFilePath(NULL),
-    m_ErpcFile(NULL), m_outputLanguage(languages_t::kCLanguage), m_codec(InterfaceDefinition::codec_t::kNotSpecified), m_addPrefix(false)
+    m_ErpcFile(NULL), m_outputLanguage(languages_t::kCLanguage), m_codec(InterfaceDefinition::codec_t::kNotSpecified), m_addPrefix(false), m_isDelete(false)
     {
         // create logger instance
         m_logger = new StdoutLogger();
@@ -252,6 +256,12 @@ public:
                     break;
                 }
 
+                case 'd':
+                {
+                    m_isDelete = true;
+                    break;
+                }
+
                 case 'a':
                 {
                     m_addPrefix = true;
@@ -332,6 +342,12 @@ public:
             if (!m_outputFilePath)
             {
                 m_outputFilePath = "";
+            }
+
+            if (m_isDelete)
+            {
+                deleteGeneratedFiles();
+                return 0;
             }
 
             // Parse and build definition model.
@@ -423,6 +439,491 @@ public:
                 break;
             }
         }
+    }
+
+    /*!
+     * @brief Delete erpc generated files.
+     *
+     * Delete erpc generated files include output paths.
+     */
+    void deleteGeneratedFiles()
+    {
+        char *programName = getProgramNameFromERPCFile();
+        if (programName)
+        {
+            if (strcmp(m_outputFilePath, ""))
+            {
+                /* Directly remove folder */
+                removeDirectory(m_outputFilePath);
+            }
+            else
+            {
+                char *outputDirName = getOutputDirFromFile();
+                if (outputDirName)
+                {
+                    // if output path included in erpc file, directly delete the path
+                    removeDirectory(outputDirName);
+                    free(outputDirName);
+                }
+                else
+                {
+                    /* Remove related files */
+                    removeFilesByGroupOrProgram(programName);
+                    removeImportERPCFilesRecursive(m_ErpcFile, programName);
+                }
+            }
+            free(programName);
+        }
+    }
+private:
+    /*!
+     * @brief if the line is a comment sentence.
+     *
+     * @param[in] buffer the input line buffer.
+     *
+     * @retval true if the line is a comment sentence.
+     * @retval false if the line is not a comment sentence.
+     *
+     */
+    static bool isCommentLine(const char *buffer)
+    {
+        const char *end = buffer + strlen(buffer);
+
+        // skip all space
+        while ((*buffer == ' ') && (buffer < end))
+        {
+            buffer++;
+        }
+
+        if (buffer == end)
+        {
+            return false;
+        }
+
+        return (*buffer == '#') ? true : false;
+    }
+
+    /*!
+     * @brief Get next target char offset.
+     *
+     * @param[in] start is the pointer to the begging of buffer.
+     * @param[in] end is the pointer to the end of buffer.
+     * @param[in] target is the char need to be found in buffer.
+     *
+     * Get the target char offset from start to end.
+     *
+     * @retval NULL if there is no target char in buffer.
+     * @retval non-NULL if there is target char in buffer, and return the pointer.
+     *
+     */
+    static char* getNextTargetCharPos(char *start, char *end, char target)
+    {
+        while (*start != target && (start < end))
+        {
+            start++;
+        }
+
+        if (start == end)
+        {
+            // can't find any target char
+            return NULL;
+        }
+
+        return start;
+    }
+
+    /*!
+     * @brief Get property value from line buffer.
+     *
+     * @param[in] strLine is the input line buffer.
+     * @param[in] keyWord is the property name.
+     *
+     * Get the property value from line buffer indexed by keyWord.
+     *
+     * @retval NULL if there is no propery value in buffer.
+     * @retval non-NULL if there is propety value in buffer, and return the value pointer.
+     *
+     */
+    static char* getPropertyValue(char *strLine, const char *keyWord)
+    {
+        char *target = strstr(strLine, keyWord);
+        char *end = strLine + strlen(strLine);
+        char *nameFirstChar = NULL;
+
+        if (target == NULL)
+        {
+            // can't find any program keyword in strLine
+            return NULL;
+        }
+
+        target += strlen(keyWord);
+
+        target = getNextTargetCharPos(target, end, '(');
+        if (target == NULL)
+        {
+            return NULL;
+        }
+
+        target = getNextTargetCharPos(target, end, '"');
+        if (target == NULL)
+        {
+            return NULL;
+        }
+
+        target++;
+        nameFirstChar = target;
+
+        nameFirstChar = getNextTargetCharPos(nameFirstChar, end, '"');
+        if (nameFirstChar == NULL)
+        {
+            return NULL;
+        }
+
+        *nameFirstChar = 0;
+
+        return strdup(target);
+    }
+
+    /*!
+     * @brief Get property from erpcFile.
+     *
+     * @param[in] func User defined function for getting property.
+     *
+     * Get property from erpc file through user defined function
+     *
+     * @retval NULL if there is no property in buffer.
+     * @retval non-NULL if there is property in buffer and return the property pointer.
+     *
+     */
+    char* getPropertyFromERPCFile(char* (*func)(char *))
+    {
+        FILE *fd;
+        char buffer[512];
+        char *target = NULL;
+
+        fd = fopen(m_ErpcFile, "r");
+        if (fd == NULL)
+        {
+            return NULL;
+        }
+
+        while (NULL != fgets(buffer, sizeof(buffer), fd))
+        {
+            if (feof(fd))
+                break;
+            if (isCommentLine(buffer))
+            {
+                continue;
+            }
+            else if (target = func(buffer))
+            {
+                break;
+            }
+        }
+
+        fclose(fd);
+        return target;
+    }
+
+    /*!
+     * @brief get program name from line buffer
+     *
+     * @param[in] strLine is the input line buffer.
+     *
+     * Parse program name from line buffer, get return.
+     *
+     * @retval NULL if no valid program name.
+     * @retval non-NULL if there is valid program name.
+     *
+     */
+    static char* getProgramName(char *strLine)
+    {
+        char *target = strstr(strLine, "program ");
+        char *end = strLine + strlen(strLine);
+        char *nameFirstChar = NULL;
+
+        if (target == NULL)
+        {
+            // can't find any program keyword in strLine
+            return NULL;
+        }
+
+        target += strlen("program ");
+
+        while (!isalpha(*target) && (target < end))
+        {
+            target++;
+        }
+
+        if (target == end)
+        {
+            // can't find any alphabet or number after program
+            return NULL;
+        }
+
+        nameFirstChar = target;
+        while ((isalnum(*nameFirstChar) || (*nameFirstChar == '-') || (*nameFirstChar == '_')) &&
+               (nameFirstChar < end) && (*nameFirstChar != '#'))
+        {
+            nameFirstChar++;
+        }
+
+        *nameFirstChar = 0;
+
+        return strdup(target);
+    }
+
+    /*!
+     * @brief Get program name from erpcFile
+     *
+     * Get the program name from the head erpc file, import files don't have program keyword.
+     *
+     * @retval NULL if there is no valid program name.
+     * @retval non-NULL if there is a valid program name.
+     *
+     */
+    char *getProgramNameFromERPCFile()
+    {
+        return getPropertyFromERPCFile(getProgramName);
+    }
+
+    /*!
+     * @brief Get group name from line buffer.
+     *
+     * @param[in] strLine is the input buffer.
+     *
+     * @retval NULL if there is no valid group name.
+     * @retval non-NULL if there is a valid group name and return the pointer.
+     *
+     */
+    static char* getGroupName(char *strLine)
+    {
+        return getPropertyValue(strLine, "@group");
+    }
+
+    /*!
+     * @brief Get output dir name from line buffer.
+     *
+     * @param[in] strLine is the input buffer.
+     *
+     * @retval NULL if there is no valid output_dir name.
+     * @retval non-NULL if there is a valid output_dir name and return the pointer.
+     *
+     */
+    static char* getOutputDirName(char *strLine)
+    {
+        return getPropertyValue(strLine, "@output_dir");
+    }
+
+    /*!
+     * @brief Get output directory name from erpcFile
+     *
+     * Get the out dir name from the head erpc file, import files don't have output_dir keyword.
+     *
+     * @retval NULL if there is no valid output_dir name.
+     * @retval non-NULL if there is a valid output_dir name.
+     *
+     */
+    char* getOutputDirFromFile()
+    {
+        return getPropertyFromERPCFile(getOutputDirName);
+    }
+
+    /*!
+     * @brief Remove the full directory indicated by path.
+     *
+     * @param[in] path is the input path.
+     *
+     * Delete the whole directory including the sub dir files.
+     *
+     */
+    static void removeDirectory(const char *path)
+    {
+        struct dirent *entry = NULL;
+        DIR *dir = NULL;
+
+        dir = opendir(path);
+        if (dir == NULL)
+        {
+            return;
+        }
+        while (entry = readdir(dir))
+        {
+            DIR *sub_dir = NULL;
+            FILE *file = NULL;
+            char abs_path[PATH_MAX+1] = {0};
+            if(*(entry->d_name) != '.')
+            {
+                sprintf(abs_path, "%s/%s", path, entry->d_name);
+                if(sub_dir = opendir(abs_path))
+                {
+                    closedir(sub_dir);
+                    removeDirectory(abs_path);
+                }
+                else
+                {
+                    if(file = fopen(abs_path, "r"))
+                    {
+                        fclose(file);
+                        remove(abs_path);
+                    }
+                }
+            }
+        }
+        remove(path);
+    }
+
+    /*!
+     * @brief Remove the generated files by program name or group name.
+     *
+     * @param[in] pgname is the program or group name.
+     *
+     * Delete the generated files under current path.
+     *
+     */
+    void removeFilesByGroupOrProgram(const char *pgname)
+    {
+         // c language will gen xxx_server.cpp, c_xxx_server.cpp, xxx_interface.cpp xxx_server.hpp
+         // c_xxx_server.h xxx_interface.hpp xxx_common.h xxx_common.hpp xxx_client.cpp
+         // c_xxx_client.cpp xxx_client.hpp c_xxx_client.h (xxx is interfaceName)
+        if (m_outputLanguage == languages_t::kCLanguage) {
+            char fname[PATH_MAX+1] = {0};
+            const char *c_gen_file_prefix_str [] = {
+                "",
+                "c_",
+                "",
+                "",
+                "c_",
+                "",
+                "",
+                "",
+                "",
+                "c_",
+                "",
+                "c_",
+            };
+            const char *c_gen_file_suffix_str [] = {
+                "_server.cpp",
+                "_server.cpp",
+                "_interface.cpp",
+                "_server.hpp",
+                "_server.h",
+                "_interface.hpp",
+                "_common.h",
+                "_common.hpp",
+                "_client.cpp",
+                "_client.cpp",
+                "_client.hpp",
+                "_client.h",
+            };
+
+            for (size_t i = 0; i < (sizeof(c_gen_file_suffix_str) / sizeof(const char *)); ++i) {
+                strcpy(fname, c_gen_file_prefix_str[i]);
+                strcat(fname, pgname);
+                strcat(fname, c_gen_file_suffix_str[i]);
+                remove(fname);
+            }
+        }
+        // python only generate xxx folder and __init__.py (xxx is interfaceName)
+        else if (m_outputLanguage == languages_t::kPythonLanguage)
+        {
+            removeDirectory(pgname);
+            remove("__init__.py");
+        }
+        // Java only generate xxx folder (xxx is interfaceName)
+        else if (m_outputLanguage == languages_t::kJavaLanguage)
+        {
+            removeDirectory(pgname);
+        }
+    }
+
+    /*!
+     * @brief Get the imported file path.
+     *
+     * @param[in] strLine is the line buffer.
+     *
+     * Get the imported sub erpc file path by parsing erpc file.
+     *
+     * @retval NULL if there is no import file.
+     * @retval non-NULL if there is valid import file.
+     *
+     */
+    static char* getImportFilePath(char *strLine)
+    {
+        char *target = strstr(strLine, "import ");
+        char *end = strLine + strlen(strLine);
+        char *nameFirstChar = NULL;
+
+        if (target == NULL)
+        {
+            // can't find any import keyword in strLine
+            return NULL;
+        }
+
+        target += strlen("import ");
+
+        target = getNextTargetCharPos(target, end, '"');
+        if (target == NULL)
+        {
+            return NULL;
+        }
+
+        target++;
+        nameFirstChar = target;
+
+        nameFirstChar = getNextTargetCharPos(nameFirstChar, end, '"');
+        if (nameFirstChar == NULL)
+        {
+            // no last "
+            return NULL;
+        }
+
+        *nameFirstChar = 0;
+
+        return strdup(target);
+    }
+
+    /*!
+     * @brief Remove the erpc generated files which are import by root erpc file.
+     *
+     * @param[in] rootName is the erpc file name.
+     * @param[in] programName is the program name given by root erpc file.
+     *
+     * Delete the files recursivly generated by imported erpc files.
+     *
+     */
+    void removeImportERPCFilesRecursive(const char *rootName, const char *programName)
+    {
+        FILE *fd;
+        char buffer[512];
+        char *target = NULL;
+
+        fd = fopen(rootName, "r");
+        if (fd == NULL)
+        {
+            return;
+        }
+
+        while (NULL != fgets(buffer, sizeof(buffer), fd))
+        {
+            if (feof(fd))
+                break;
+            if (target = getGroupName(buffer))
+            {
+                strcpy(buffer, programName);
+                strcat(buffer, "_");
+                strcat(buffer, target);
+                removeFilesByGroupOrProgram(buffer);
+                free(target);
+            }
+            else if (target = getImportFilePath(buffer))
+            {
+                removeImportERPCFilesRecursive(target, programName);
+                free(target);
+            }
+        }
+
+        fclose(fd);
     }
 };
 
